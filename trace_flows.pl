e3079a1415be69c780961c464dc73cd3471614b7
Jason Bittel <jason.bittel@gmail.com>
2005-11-02 06:18:57 +0000

    Major improvements to trace_flows.pl: removed debugging code, added one-to-one/one-to-many flow parsing options, tweaked flow discard limit, sped up internal data representation and many other minor tweaks and fixes

--- trace_flows.pl
+++ trace_flows.pl
@@ -18,9 +18,7 @@ my $PATTERN = "\t";
 my $PROG_NAME = "trace_flows.pl";
 my $PROG_VER = "0.0.2";
 my $FLOW_TIMEOUT = 300; # Timeout for flows, in seconds
-my $FLOW_DISCARD = 1; # Discard flows below this length
-my $DEBUG = 0; # Debug flag for helpful print messages
-my $DEBUG2 = 0; # Debug flag for even more helpful print messages
+my $FLOW_DISCARD = 5; # Discard flows below this length
 
 # -----------------------------------------------------------------------------
 # GLOBAL VARIABLES
@@ -36,6 +34,8 @@ my @input_files;
 my $output_file;
 my $flow_timeout;
 my $flow_discard;
+my $flow_one2one;
+my $flow_one2many;
 
 # -----------------------------------------------------------------------------
 # Main Program
@@ -54,7 +54,6 @@ sub parse_flows {
         my $curr_file; # Current input file
         my ($timestamp, $src_ip, $dst_ip, $hostname, $uri);
         my $flow_key;
-        my $flow_id = 0;
         my $key;
         my $epochstamp;
 
@@ -67,13 +66,16 @@ sub parse_flows {
 
                 foreach $curr_line (<INFILE>) {
                         chomp $curr_line;
-                        $curr_line =~ tr/\x80-\xFF//d; # Strip non-printable chars
                         next if $curr_line eq "";
 
                         ($timestamp, $src_ip, $dst_ip, $hostname, $uri) = split(/$PATTERN/, $curr_line);
 
-                        next if (!$src_ip or !$dst_ip or !$hostname);
-                        $flow_key = $src_ip.'-'.$dst_ip;
+                        # Determine the type of flow requested
+                        if ($flow_one2many) {
+                                $flow_key = $src_ip;
+                        } elsif ($flow_one2one) {
+                                $flow_key = $src_ip.'-'.$dst_ip;
+                        }
 
                         # Convert timestamp of current record to epoch seconds
                         $timestamp =~ /(\d\d)\/(\d\d)\/(\d\d\d\d) (\d\d)\:(\d\d)\:(\d\d)/;
@@ -81,35 +83,28 @@ sub parse_flows {
 
                         # Let's make magic happen here, baby
                         if (!exists $flow_info{$flow_key}) {
-                                print "New flow #$flow_id\n" if $DEBUG;
-                                $flow_info{$flow_key}->{"id"} = $flow_id++;
-                                $flow_info{$flow_key}->{"src_ip"} = $src_ip;
-                                $flow_info{$flow_key}->{"dst_ip"} = $dst_ip;
-                                $flow_info{$flow_key}->{"hostname"} = $hostname;
                                 $flow_info{$flow_key}->{"start_time"} = $timestamp;
                                 $flow_info{$flow_key}->{"end_time"} = $timestamp;
                                 $flow_info{$flow_key}->{"start_epoch"} = $epochstamp;
                                 $flow_info{$flow_key}->{"end_epoch"} = $epochstamp;
                                 $flow_info{$flow_key}->{"length"} = 1;
 
-                                push(@{$flow_data{$flow_key}}, $timestamp.$PATTERN.$uri);
+                                push(@{$flow_data{$flow_key}}, $curr_line);
                                 #$#{$flow_data{$flow_key}} = 1000;
                         } else {
                                 $flow_info{$flow_key}->{"end_time"} = $timestamp;
                                 $flow_info{$flow_key}->{"end_epoch"} = $epochstamp;
                                 $flow_info{$flow_key}->{"length"}++;
 
-                                push(@{$flow_data{$flow_key}}, $timestamp.$PATTERN.$uri);
+                                push(@{$flow_data{$flow_key}}, $curr_line);
                         }
 
                         # Timeout old flows
                         foreach $key (keys %flow_info) {
-                                print "." if $DEBUG2;
                                 if (($epochstamp - $flow_info{$key}->{"end_epoch"}) > $flow_timeout) {
                                         &timeout_flow($key);
                                 }
                         }
-                        print "\n" if $DEBUG2;
                 }
 
                 close(INFILE);
@@ -129,15 +124,13 @@ sub timeout_flow {
         my $flow_key = shift;
 
         # Discard brief flows
-        if ($flow_info{$flow_key}->{'length'} <= 1) {
-                print "Flow $flow_info{$flow_key}->{'id'} discarded\n" if $DEBUG;
+        if ($flow_info{$flow_key}->{'length'} <= $flow_discard) {
                 delete $flow_info{$flow_key};
                 delete $flow_data{$flow_key};
 
                 return;
         }
 
-        print "Flow $flow_info{$flow_key}->{'id'} concluded\n" if $DEBUG;
         &print_flow($flow_key);
 
         delete $flow_info{$flow_key};
@@ -149,28 +142,20 @@ sub timeout_flow {
 # -----------------------------------------------------------------------------
 sub print_flow {
         my $flow_key = shift;
-        my ($timestamp, $src_ip, $dst_ip, $hostname, $uri);
-        my $key;
         my $line;
         my $metadata;
 
         open(OUTFILE, ">>$output_file") || die "\nError: cannot open $output_file - $!\n";
 
         # Print flow header line
-        $metadata = sprintf("%d!%d!%s!%s", $flow_info{$flow_key}->{'id'},
-                                           $flow_info{$flow_key}->{'length'},
-                                           $flow_info{$flow_key}->{'start_time'},
-                                           $flow_info{$flow_key}->{'end_time'});
+        $metadata = sprintf("%d!%s!%s", $flow_info{$flow_key}->{'length'},
+                                        $flow_info{$flow_key}->{'start_time'},
+                                        $flow_info{$flow_key}->{'end_time'});
         print OUTFILE ">>> $metadata " . '>' x (75 - length($metadata)) . "\n";
 
         # Print flow data lines
-        $src_ip = $flow_info{$flow_key}->{"src_ip"};
-        $dst_ip = $flow_info{$flow_key}->{"dst_ip"};
-        $hostname = $flow_info{$flow_key}->{"hostname"};
         foreach $line ( @{$flow_data{$flow_key}} ) {
-                ($timestamp, $uri) = split(/$PATTERN/, $line);
-
-                print OUTFILE "$timestamp\t$src_ip\t$dst_ip\t$hostname\t$uri\n";
+                print OUTFILE "$line\n";
         }
 
         # Print flow footer line
@@ -183,7 +168,7 @@ sub print_flow {
 # Retrieve and process command line arguments
 # -----------------------------------------------------------------------------
 sub get_arguments {
-        getopts('d:ho:t:', \%opts) or &print_usage();
+        getopts('1d:hmo:t:', \%opts) or &print_usage();
 
         # Print help/usage information to the screen if necessary
         &print_usage() if ($opts{h});
@@ -194,11 +179,17 @@ sub get_arguments {
         $flow_timeout = $FLOW_TIMEOUT unless ($flow_timeout = $opts{t});
         $flow_discard = $FLOW_DISCARD unless ($flow_discard = $opts{d});
         $output_file = 0 unless ($output_file = $opts{o});
+        $flow_one2one = 0 unless ($flow_one2one = $opts{1});
+        $flow_one2many = 0 unless ($flow_one2many = $opts{m});
 
         # Check for required options and combinations
         if (!$output_file) {
                 print "\nError: no output file provided\n";
                 &print_usage();
+        }
+        if (!$flow_one2one && !$flow_one2many) {
+                print "\nWarning: no flow output type specified, defaulting to one-to-many\n";
+                $flow_one2many = 1;
         }
 }
 
@@ -208,6 +199,6 @@ sub get_arguments {
 sub print_usage {
         die <<USAGE;
 $PROG_NAME version $PROG_VER
-Usage: $PROG_NAME [-h] [-o file] [-t timeout]
+Usage: $PROG_NAME [-1hm] [-d discard] [-o file] [-t timeout]
 USAGE
 }
