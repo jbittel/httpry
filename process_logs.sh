8664c0ccbd299daa24dc1538eec3d3fe653d63f3
Jason Bittel <jason.bittel@gmail.com>
2005-12-23 00:35:24 +0000

    Updated shell scripts to reflect changes to httpry

--- process_logs.sh
+++ process_logs.sh
@@ -16,22 +16,23 @@
 # --------------------
 # --- MODIFY THESE ---
 # --------------------
-# Change these values to reflect your particular setup
+# Change these values to reflect your particular setup. If you do not
+# provide an option here, be sure to remove that parameter in the script
+# below.
 
 tools_dir=""     # Change this to the location of the perl tool scripts
 logs_dir=""      # Change this to where you want to store your logs
 email_addr=""    # Email address used by parse_log.pl reports
-user=""          # User to run httpry as
-group=""         # Group of the above user
-log_fn="out.log" # Default file name for active log file
-offense_fn=""    # File name of content checks file; put in tools dir
+content_fn=""    # File name of content checks file; put in tools dir
+config_file=""   # Path to httpry config file
 
 # --------------------
 
-parse_fn="`date +%-m-%-d-%Y`.log" # This is the default format used by rotate_log.pl
+log_fn="out.log"                  # Default file name for active log file
+parse_fn="`date +%-m-%-d-%Y`.log" # This is the date format used by rotate_log.pl
 
 # Stop the httpry service if it is running
-if [ -e "/var/run/httpry.pid" ]; then
+if [ -r "/var/run/httpry.pid" ]; then
         kill `cat /var/run/httpry.pid`
 else
         killall httpry
@@ -42,15 +43,12 @@ if [ -e "$tools_dir/$log_fn" ]; then
         perl $tools_dir/rotate_log.pl -ct -i $logs_dir/$log_fn -d $logs_dir
 fi
 
-# Start the httpry service
-httpry -d -u $user -o $logs_dir/$log_fn
+# Restart the httpry service
+httpry -c $config_file
 
 # Process new log file data
 if [ -e "$logs_dir/$parse_fn" ]; then
         perl $tools_dir/parse_log.pl -s -o $logs_dir/$parse_fn.txt -c 20 -e $email_addr $logs_dir/$parse_fn
         perl $tools_dir/trace_flows.pl -m -o $logs_dir/$parse_fn.flows $logs_dir/$parse_fn
-        perl $tools_dir/parse_flows.pl -sx -l $tools_dir/$offense_fn -d $logs_dir -o $logs_dir/$parse_fn.txt -e $email_addr $logs_dir/$parse_fn.flows
+        perl $tools_dir/parse_flows.pl -sx -l $tools_dir/$content_fn -d $logs_dir -o $logs_dir/$parse_fn.txt -e $email_addr $logs_dir/$parse_fn.flows
 fi
-
-# Change ownership of logs directory.
-chown $user:$group $logs_dir/*
