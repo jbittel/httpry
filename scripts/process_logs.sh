c97cb608772954169b62b54f450c1e79a46bf9f2
Jason Bittel <jason.bittel@gmail.com>
2006-10-04 08:23:00 +0000

    Updated shell scripts to match program changes and to add additional error checking

--- scripts/process_logs.sh
+++ scripts/process_logs.sh
@@ -8,43 +8,47 @@
 # different directory, restart httpry and then process the new log
 # file. This demonstrates the majority of the functionality included
 # in these different tools, and should help in modifying them to
-# your particular use.  Note that this file is merely an example of
-# how to automate this process; most of these programs have additional
-# options that are not shown here.
+# your particular use.
 #
 
-# --------------------
-# --- MODIFY THESE ---
-# --------------------
-# Change these values to reflect your particular setup. If you do not
-# provide an option here, be sure to remove that parameter in the script
-# below.
+# !!! Change these values to reflect your particular setup !!!
 
-tools_dir=""  # Change this to the location of the perl tool scripts
-logs_dir=""   # Change this to where you want to store your logs
-email_addr="" # Email address used by parse_log.pl reports
-content_fn="" # File name of content checks file; put in tools dir
+tools_dir=""      # Change this to the location of the perl tool scripts
+logs_dir=""       # Change this to where you want to store your logs
+log_fn="out.log"  # Default file name for active log file
 
-# --------------------
 
-log_fn="out.log"                  # Default file name for active log file
-parse_fn="`date +%-m-%-d-%Y`.log" # This is the date format used by rotate_log.pl
-out_fn="`date +%-m-%-d-%Y`"       # Use current date as base filename for output files
+die() {
+        if [ -n "${1}" ] ; then
+                echo "Error: ${1}" >&2
+        fi
+
+        exit 1
+}
+
+if [ ! -d "${tools_dir}" ] ; then
+        die "Tools directory is not set or is invalid"
+fi
+if [ ! -d "${logs_dir}" ] ; then
+        die "Log file directory is not set or is invalid"
+fi
+if [ ! -r "${tools_dir}/${log_fn}" ] ; then
+        die "Log file does not exist or is unreadable"
+fi
 
 # Stop the httpry service if it is running
 /etc/rc.d/rc.httpry stop
 
 # Compress/move/purge log files
-if [ -e "$tools_dir/$log_fn" ]; then
-        perl $tools_dir/rotate_log.pl -ct -i $tools_dir/$log_fn -d $logs_dir
-fi
+perl ${tools_dir}/rotate_log.pl -ct -i ${tools_dir}/${log_fn} -d ${logs_dir}
 
 # Restart the httpry service
 /etc/rc.d/rc.httpry start
 
 # Process new log file data; make sure appropriate plugins are
 # enabled/disabled for parse_log.pl
-if [ -e "$logs_dir/$parse_fn" ]; then
-        perl $tools_dir/client_flows.pl -e $email_addr -l $tools_dir/$content_fn -o $logs_dir/$out_fn-content.txt $logs_dir/$parse_fn
-        perl $tools_dir/parse_log.pl -p $tools_dir/plugins $logs_dir/$parse_fn
+if [ `ls -l ${logs_dir}/*.log | wc -l` -gt "0" ] ; then
+        perl ${tools_dir}/parse_log.pl -p ${tools_dir}/plugins ${logs_dir}/*.log
+else
+        die "No log files found in ${logs_dir}"
 fi
