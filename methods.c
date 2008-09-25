6e0453da92b2e130331a4c7c0930e18fbdf4aea7
Jason Bittel <jason.bittel@gmail.com>
2008-09-25 17:33:29 +0000

    Reverted methods.c to using the simpler unbalanced binary tree data structure

--- methods.c
+++ methods.c
@@ -9,6 +9,16 @@
 */
 
 /*
+  The methods data structure is an unbalanced binary tree. All
+  packets are checked to see if they have a method contained
+  here; any packets that do not will be ignored.
+
+  This doesn't use a hash because the length of the potential
+  method is not known. At this point in the main processing
+  loop the packet data is still in a static buffer, so this
+  gives us a simpler solution. Perhaps at some point the flow
+  of the packet processing will be improved and we can switch
+  to a more traditional lookup table approach.
 */
 
 #include <ctype.h>
@@ -19,22 +29,16 @@
 #include "methods.h"
 #include "utility.h"
 
-#define HASHSIZE 20
-
-typedef struct node NODE;
-struct node {
-        char *name;
-        NODE *next, *list;
+typedef struct method_node METHOD_NODE;
+struct method_node {
+        char *method;
+        METHOD_NODE *left, *right;
 };
 
-static NODE *methods[HASHSIZE];
-static NODE *head = NULL;
+static METHOD_NODE *methods = NULL;
 
-NODE *insert_method(char *str);
-void free_node(NODE *node);
-NODE *method_lookup(const char *str, int len);
-unsigned hash_method_string(const char *str, int len);
-void free_methods();
+int insert_method(char *str);
+void free_node(METHOD_NODE *node);
 
 /* Parse and insert methods from input string */
 void parse_methods_string(char *str) {
@@ -66,141 +70,88 @@ void parse_methods_string(char *str) {
         if (num_methods == 0)
                 LOG_DIE("No valid methods found in string");
 
-#ifdef DEBUG
-        int j, num_buckets = 0, num_chain, max_chain = 0;
-        NODE *node;
-
-        for (j = 0; j < HASHSIZE; j++) {
-                if (methods[j]) num_buckets++;
-
-                num_chain = 0;
-                for (node = methods[j]; node != NULL; node = node->next) num_chain++;
-                if (num_chain > max_chain) max_chain = num_chain;
-        }
-
-        PRINT("----------------------------");
-        PRINT("Hash buckets:       %d", HASHSIZE);
-        PRINT("Nodes inserted:     %d", num_methods);
-        PRINT("Buckets in use:     %d", num_buckets);
-        PRINT("Hash collisions:    %d", num_methods - num_buckets);
-        PRINT("Longest hash chain: %d", max_chain);
-        PRINT("----------------------------");
-#endif
-
         return;
 }
 
 /* Insert a new method into the structure */
-NODE *insert_method(char *method) {
-        NODE *node;
-        static NODE *prev = NULL;
-        unsigned hashval;
-
+int insert_method(char *method) {
+        METHOD_NODE **node = &methods;
+        int cmp;
+        
 #ifdef DEBUG
         ASSERT(method);
         ASSERT(strlen(method) > 0);
 #endif
- 
-        if ((node = method_lookup(method, strlen(method))) == NULL) {
-                if ((node = (NODE *) malloc(sizeof(NODE))) == NULL)
-                        LOG_DIE("Cannot allocate memory for method node");
-
-                hashval = hash_method_string(method, strlen(method));
-
-#ifdef DEBUG
-        ASSERT((hashval >= 0) && (hashval < HASHSIZE));
-#endif
-
-                node->next = methods[hashval];
-                methods[hashval] = node;
-        } else {
-                WARN("Method '%s' already provided", method);
-                return NULL;
+        
+        while (*node) {
+                cmp = str_compare(method, (*node)->method, strlen((*node)->method));
+                if (cmp > 0) {
+                        node = &(*node)->right;
+                } else if (cmp < 0) {
+                        node = &(*node)->left;
+                } else {
+                        WARN("Method '%s' already provided", method);
+                        
+                        return 0;
+                }
         }
-
-        if ((node->name = (char *) malloc(strlen(method) + 1)) == NULL) {
+        
+        if ((*node = (METHOD_NODE *) malloc(sizeof(METHOD_NODE))) == NULL) {
+                LOG_DIE("Cannot allocate memory for method node");
+        }
+        
+        if (((*node)->method = (char *) malloc(strlen(method) + 1)) == NULL) {
                 LOG_DIE("Cannot allocate memory for method string");
         }
-        strcpy(node->name, method);
-
-        /* Update the linked list pointers */
-        if (prev) prev->list = node;
-        prev = node;
-        if (!head) head = node;
-
-        return node;
+        
+        strcpy((*node)->method, method);
+        (*node)->left = (*node)->right = NULL;
+        
+        return 1;
 }
 
 /* Search parameter string for a matching method */
 int is_request_method(const char *str) {
-        NODE *node;
-        int len;
-        char *c;
-
+        METHOD_NODE *node = methods;
+        int cmp;
+        
 #ifdef DEBUG
+        ASSERT(node);
         ASSERT(str);
 #endif
-
+        
         if (strlen(str) == 0) return 0;
-
-        c = strchr(str, ' ');
-        if (!c) return 0;
-        len = c - str;
-
-        if ((node = method_lookup(str, len))) return 1;
-
+        
+        while (node) {
+                cmp = str_compare(str, node->method, strlen(node->method));
+                if (cmp > 0) {
+                        node = node->right;
+                } else if (cmp < 0) {
+                        node = node->left;
+                } else {
+                        return 1;
+                }
+        }
+        
         return 0;
 }
 
-/* Lookup a particular node in hash; return pointer to node
-   if found, NULL otherwise */
-NODE *method_lookup(const char *str, int len) {
-        NODE *node;
-
-#ifdef DEBUG
-        ASSERT(str);
-        ASSERT(strlen(str) > 0);
-        ASSERT(len);
-        ASSERT((hash_method_string(str, len) >= 0) && (hash_method_string(str, len) < HASHSIZE));
-#endif
-
-        for (node = methods[hash_method_string(str, len)]; node != NULL; node = node->next)
-                if (str_compare(str, node->name, strlen(node->name)) == 0)
-                        return node;
-
-        return NULL;
-}
-
-/* Use the djb2 hash function; supposed to be good for strings */
-unsigned hash_method_string(const char *str, int len) {
-        unsigned hashval;
-
-#ifdef DEBUG
-        ASSERT(str);
-        ASSERT(strlen(str) > 0);
-        ASSERT(len);
-#endif
-
-        for (hashval = 5381; (--len > 0) && (*str != '\0'); str++)
-                hashval = (hashval * 33) ^ tolower(*str);
-
-        return hashval % HASHSIZE;
-}
-
-/* Free all allocated memory for methods; only called at program termination */
+/* Wrapper function to free allocated memory at program termination */
 void free_methods() {
-        NODE *prev, *curr;
+        free_node(methods);
+        
+        return;
+}
 
-        if (!head) return;
-
-        curr = head;
-        while (curr) {
-                prev = curr;
-                curr = curr->list;
-
-                free(prev->name);
-                free(prev);
-        }
-
+/* Recursively free all children of the parameter node */
+void free_node(METHOD_NODE *node) {
+        if (!node) return;
+        
+        free_node(node->left);
+        free_node(node->right);
+        
+        free(node->method);
+        free(node);
+        
         return;
 }
