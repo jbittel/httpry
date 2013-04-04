7eab7d8ba4275d5793eb5f199da327e5969006b1
Jason Bittel <jason.bittel@gmail.com>
2013-04-04 09:53:35 -0700

    Update copyright year

--- scripts/plugins/common_log.pm
+++ scripts/plugins/common_log.pm
@@ -3,7 +3,7 @@
 #  httpry - HTTP logging and information retrieval tool
 #  ----------------------------------------------------
 #
-#  Copyright (c) 2005-2012 Jason Bittel <jason.bittel@gmail.com>
+#  Copyright (c) 2005-2013 Jason Bittel <jason.bittel@gmail.com>
 #
 
 package common_log;
